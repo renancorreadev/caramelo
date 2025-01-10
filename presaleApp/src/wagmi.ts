1f91106929e350206f2e831e0316bd28b0d5a7a3
renancorreadev <skynancemusic@gmail.com>
2025-01-10 10:23:12 -0300

    feat: update

--- presaleApp/src/wagmi.ts
+++ presaleApp/src/wagmi.ts
@@ -3,18 +3,40 @@ import {
   rainbowWallet,
   walletConnectWallet,
   binanceWallet,
-  coinbaseWallet
+  imTokenWallet,
+  metaMaskWallet,
+  trustWallet,
+  ledgerWallet,
+  coinbaseWallet,
+  injectedWallet,
 } from '@rainbow-me/rainbowkit/wallets';
 import { bsc } from 'viem/chains';
 
 import { createConfig, http } from 'wagmi';
 
 
+function walletConnect() {
+
+  return walletConnectWallet({
+    projectId: '1874f7d69584a7d65db6382fd0760fcf',
+  });
+}
+
 const connectors = connectorsForWallets(
   [
     {
       groupName: 'Recommended',
-      wallets: [rainbowWallet, walletConnectWallet, binanceWallet, coinbaseWallet],
+      wallets: [
+        rainbowWallet, 
+        walletConnect,
+        binanceWallet, 
+        coinbaseWallet,
+        imTokenWallet,
+        metaMaskWallet,
+        trustWallet,
+        ledgerWallet,
+        injectedWallet
+      ],
     },
   ],
   {
