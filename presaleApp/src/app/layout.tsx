59e6c93f0444c9432ef6b9566ef308ed36767c9e
renancorreadev <skynancemusic@gmail.com>
2025-01-12 12:19:18 -0300

    update

--- presaleApp/src/app/layout.tsx
+++ presaleApp/src/app/layout.tsx
@@ -1,26 +1,27 @@
-import type { Metadata } from "next";
-import { Header, Footer } from "../shared";
+import Script from 'next/script';
+import type { Metadata } from 'next';
+import { Header, Footer } from '../shared';
 import { Comic_Neue } from 'next/font/google';
-import "./globals.css";
-import "../css/all.min.css";
-import "../css/aos.css";
-import "../css/bootstrap.min.css";
-import "../css/flag-icon.min.css";
-import "../css/main.css";
+import './globals.css';
+import '../css/all.min.css';
+import '../css/aos.css';
+import '../css/bootstrap.min.css';
+import '../css/flag-icon.min.css';
+import '../css/main.css';
 
 import '@rainbow-me/rainbowkit/styles.css';
-import { Providers } from "./providers";
+import { Providers } from './providers';
 
 const comicNeue = Comic_Neue({
   subsets: ['latin'],
-  weight: ['400', '700'], 
+  weight: ['400', '700'],
   display: 'swap',
   variable: '--font-comic-neue',
 });
 
 export const metadata: Metadata = {
-  title: "Caramelo Pré venda",
-  description: "Pre venda do token Caramelo",
+  title: 'Caramelo Pré-venda',
+  description: 'Pré-venda do token Caramelo',
 };
 
 export default function RootLayout({
@@ -31,12 +32,19 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className={comicNeue.variable}>
       <body className={`antialiased`}>
+        <Script src="/js/bootstrap.bundle.min.js" strategy="afterInteractive" />
+        <Script src="/js/jquery.min.js" strategy="afterInteractive" />
+       
         <Providers>
-          <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              minHeight: '100vh',
+            }}
+          >
             <Header />
-            <main style={{ flexGrow: 1 }}>
-              {children}
-            </main>
+            <main style={{ flexGrow: 1 }}>{children}</main>
             <Footer />
           </div>
         </Providers>
--- presaleApp/src/app/page.tsx
+++ presaleApp/src/app/page.tsx
@@ -1,15 +1,33 @@
 /* eslint-disable @next/next/no-img-element */
 'use client';
 
+import { useEffect } from 'react';
+import AOS from 'aos'; // Importando o AOS do pacote npm
+import 'aos/dist/aos.css'; // Importando o CSS necessário para AOS
 import { Container } from '@/components/Container';
 import PresaleForm from '@/components/PresaleForm';
 import { useAccount } from 'wagmi';
-
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 export default function Home() {
   const { isConnected } = useAccount();
 
+  // Inicializando o AOS
+  useEffect(() => {
+    const initializeAOS = () => {
+      AOS.init({
+        duration: 1000,
+        easing: 'ease-in-out',
+        once: true,
+      });
+  
+      // Garantir que o AOS esteja sincronizado com os elementos
+      AOS.refresh();
+    };
+  
+    initializeAOS();
+  }, []);
+  
   return isConnected ? (
     <Container>
       <div className="xs:!mx-6">
@@ -26,7 +44,7 @@ export default function Home() {
         style={{
           position: 'relative',
           width: '100vw',
-          height: '100vh', // Garante altura máxima para o vídeo
+          height: '100vh', // Garante que ocupe toda a altura da tela
           overflow: 'hidden',
         }}
       >
@@ -37,9 +55,9 @@ export default function Home() {
           loop
           muted
           style={{
-            width: '100%',
-            height: '100%',
-            objectFit: 'cover',
+            width: '100vw', 
+            height: '100vh', 
+            objectFit: 'cover', 
             position: 'absolute',
             top: 0,
             left: 0,
@@ -49,7 +67,6 @@ export default function Home() {
           <source src="/media/carameloheader.mp4" type="video/mp4" />
         </video>
 
-        {/* Conteúdo centralizado no vídeo */}
         <div
           id="content"
           style={{
@@ -66,10 +83,10 @@ export default function Home() {
             justifyContent: 'center',
           }}
         >
-          {/* Imagem do cachorro */}
           <a
             href="#"
             target="_blank"
+            className='lg:!relative lg:!top-20'
             onClick={() =>
               document
                 .querySelector('#ShootingDoges')
@@ -77,29 +94,17 @@ export default function Home() {
             }
           >
             <img
-              className="dogelogo"
+              className="dogelogo lg:max-w-[450px] mb-5"
               src="/images/caramelo.svg"
               alt="Caramelo Coin"
-              style={{
-                maxWidth: '450px',
-                marginBottom: '20px',
-              }}
             />
           </a>
-
-          {/* Título */}
           <h1
             id="title"
-            className="d_text_display d_span_opacity comic-neue font-bold"
-            style={{
-              fontSize: '4rem',
-              marginBottom: '10px',
-            }}
+            className="d_text_display d_span_opacity comic-neue font-bold lg:text-7xl lg:mb-3 xs:text-5xl" 
           >
             <span id="dynamic_c">C</span>ARAMELOCOIN
           </h1>
-
-          {/* Subtítulo */}
           <p
             className="subtitle poppins fw-light text-white"
             style={{
@@ -109,38 +114,132 @@ export default function Home() {
           >
             a MEMECOIN do Brasil
           </p>
-
-          {/* Botão Connect */}
           <ConnectButton />
         </div>
       </div>
 
-      <style jsx>{`
-        /* Responsividade para dispositivos móveis */
-        @media (max-width: 768px) {
-          #video-container {
-            height: 100vh;
-          }
-
-          #video-header {
-            height: 100%;
-            width: 100%;
-            object-fit: cover;
-          }
-
-          .dogelogo {
-            max-width: 100px; /* Ajusta o tamanho do logo no mobile */
-          }
-
-          #title {
-            font-size: 2.5rem; /* Reduz o tamanho da fonte no mobile */
-          }
-
-          .subtitle {
-            font-size: 1rem; /* Ajusta o subtítulo para telas menores */
-          }
-        }
-      `}</style>
+      <main>
+        <div id="o-que-e-a-caramelo-coin" className="pt-5">
+          <div className="xs:!w-[90%]">
+            <div className="row">
+              <div className="col-md-6">
+                <div className="kabosu-content">
+                  <h2
+                    className="title display-5 text-start text-dark comic-neue"
+                    id="sobre"
+                  >
+                    O que é a Caramelo Coin?
+                  </h2>
+                  <p className="fs-3 lh-md comic-neue">
+                    Uma moeda digital que transforma a vida dos cachorros de rua &nbsp;
+                    <span className="fs-3 fw-bold text-warning comic-neue">
+                     e&nbsp; resgata a essência dos caramelos, verdadeiros ícones do
+                      Brasil.
+                    </span>
+                  </p>
+                  <p className="lh-md comic-neue">
+                    No coração da CarameloCoin, está um movimento criptográfico
+                    dedicado a apoiar ONGs que lutam diariamente para dar
+                    dignidade e um lar para os amados cachorros “caramelo”. Esta
+                    criptomoeda de código aberto utiliza a tecnologia
+                    blockchain, garantindo um sistema descentralizado e seguro
+                    de armazenamento de informações, mantendo uma rede robusta e
+                    solidária. Mais do que isso, porém, é o espírito do
+                    CarameloCoin: um projeto que une comunidade, empatia e o
+                    amor pelos cães vira-latas, ícones nacionais. Junte-se a
+                    este movimento vibrante e seja parte da mudança!
+                  </p>
+                </div>
+              </div>
+              <div className="col-md-6 parent">
+                <div>
+                  <img
+                    className="over"
+                    src="/images/caramelos.webp"
+                    alt="Caramelos"
+                    data-aos="fade-left"
+                  />
+                </div>
+                <br />
+                <br />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="kabosu-content" style={{ minWidth: '100%' }}>
+                  <h2
+                    className="title display-5 text-start text-dark comic-neue"
+                    style={{ marginBottom: '20px' }}
+                  >
+                    Como começar!
+                  </h2>
+                  <br />
+                  <br />
+                  <div className="row">
+                    <div
+                      className="col-sm-4 parent-numbers"
+                      data-aos="fade-left"
+                      data-aos-offset="100"
+                      data-aos-delay="10"
+                      data-aos-duration="1000"
+                    >
+                      <h3>
+                        <span className="badge bg-secondary">01</span> Escolha
+                        sua carteira
+                        <br />
+                        &nbsp;
+                      </h3>
+                      <p>
+                        Uma carteira Metamask ou Trust Wallet é necessária para
+                        pessoas que desejam usar, negociar ou manter
+                        CarameloCoin. Você pode escolher uma carteira e começar!
+                      </p>
+                      <br />
+                    </div>
+                    <div
+                      className="col-sm-4 parent-numbers"
+                      data-aos="fade-right"
+                      data-aos-offset="100"
+                      data-aos-delay="10"
+                      data-aos-duration="1000"
+                    >
+                      <h3>
+                        <span className="badge bg-secondary">02</span> Compre
+                        BNB
+                      </h3>
+                      <p>
+                        Compre BNB através de uma corretora centralizada como a
+                        Binance ou descentralizada como a PancakeSwap e envie os
+                        BNB para sua carteira.
+                      </p>
+                    </div>
+                    <br />
+                    <div
+                      className="col-sm-4 parent-numbers"
+                      data-aos="fade-left"
+                      data-aos-offset="100"
+                      data-aos-delay="10"
+                      data-aos-duration="1000"
+                    >
+                      <h3>
+                        <span className="badge bg-secondary">03</span> Conectar
+                        Carteira
+                        <br />
+                        &nbsp;
+                      </h3>
+                      <p>
+                        Conecte sua carteira clicando no botão &quot;Conectar
+                        Carteira&quot; e faça a troca dos seus BNB para
+                        Caramelo.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
